0cb10f2643f0b37541aa3a81c6d0d96fb4499391
Олег Воронцов <oleg-voroncov@mail.ru>
2024-02-07 16:43:57 +0300

    RoleInitializer added. Enter as admin using login: admin@gmail.com  password: booMER86@

---
+++ Data/RoleInitializer.cs
@@ -0,0 +1,31 @@
+﻿using ASP.NET_Core_MVC_App_PhoneBook.AuthApp;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_App_PhoneBook.Data
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "booMER86@";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,16 +8,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using ASP.NET_Core_MVC_App_PhoneBook.Data;
-
+using Microsoft.AspNetCore.Identity;
+using ASP.NET_Core_MVC_App_PhoneBook.AuthApp;
 
 namespace ASP.NET_Core_MVC_App_PhoneBook
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             CreateDbIfNotExists(host);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await RoleInitializer.InitializeAsync(userManager, rolesManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
             host.Run();
         }
 
